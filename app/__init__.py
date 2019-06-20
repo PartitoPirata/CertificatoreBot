707ed9a5c03cb41b0784d268ad88e6f3a300c375
Giuseppe C <peppecal+gitid@gmail.com>
2019-06-20 16:09:02 +0200

    meno serietà

--- app/__init__.py
+++ app/__init__.py
@@ -12,7 +12,7 @@ from telegram.ext import CommandHandler
 
 def help_fun(update, context):
     topic = {
-        "lince": "Lince non esiste, te lo sei immaginato.\n\nSul serio",
+        "lince": "Lince non esiste, te lo sei immaginato.",
         "lqfb": "È troppo presto per parlarne, ma è un software che gestisce le mozioni politiche del partito."
     }
 
